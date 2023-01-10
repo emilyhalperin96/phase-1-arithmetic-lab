438c329296ce475c2b45f224f09af15c3fd60ce3
emilyphalperin@gmail.com <emilyhalperin@Emilys-MBP-2.lan>
2023-01-09 20:44:45 -0500

    done

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+console.log(random); 
+
+let num3 = 4;
+let num4 = 25;
+let mod = num3 % num4;
+
+console.log(mod);
+
+let numbers = [5, 10, 15, 20];
+let max = Math.max(...numbers);
+
+console.log(max);
